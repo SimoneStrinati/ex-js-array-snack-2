e44e01740619660624ede93598745bb542fb6b25
simonestrinati <simone.strinati@outlook.it>
2026-04-17 20:14:01 +0200

    Bonus 5

--- main.js
+++ main.js
@@ -1,63 +1,67 @@
 const books = [
-  { 
-	  title: "React Billionaire", 
-	  pages: 250, 
-	  author: {
-		  name: 'Alice',
-		  age: 35
-	  },
-	  available: false,
-	  price: '101€',
-	  tags: ['advanced', 'js', 'react', 'senior']
-  },
-  { 
-	  title: "Advanced JS", 
-	  pages: 500, 
-	  author: {
-		  name: 'Bob',
-		  age: 20
-	  },
-	  available: true,
-	  price: '25€',
-	  tags: ['advanced', 'js', 'mid-senior']
-  },
-  { 
-	  title: "CSS Secrets", 
-	  pages: 320, 
-	  author: {
-		  name: 'Alice',
-		  age: 17
-	  },
-	  available: true,
-	  price: '8€',
-	  tags: ['html', 'css', 'junior']
-  },
-  { 
-	  title: "HTML Mastery", 
-	  pages: 200, 
-	  author: {
-		  name: 'Charlie',
-		  age: 50
-	  },
-	  available: false,
-	  price: '48€',
-	  tags: ['html', 'advanced', 'junior', 'mid-senior']
-  },
+    {
+        title: "React Billionaire",
+        pages: 250,
+        author: {
+            name: 'Alice',
+            age: 35
+        },
+        available: false,
+        price: '101€',
+        tags: ['advanced', 'js', 'react', 'senior']
+    },
+    {
+        title: "Advanced JS",
+        pages: 500,
+        author: {
+            name: 'Bob',
+            age: 20
+        },
+        available: true,
+        price: '25€',
+        tags: ['advanced', 'js', 'mid-senior']
+    },
+    {
+        title: "CSS Secrets",
+        pages: 320,
+        author: {
+            name: 'Alice',
+            age: 17
+        },
+        available: true,
+        price: '8€',
+        tags: ['html', 'css', 'junior']
+    },
+    {
+        title: "HTML Mastery",
+        pages: 200,
+        author: {
+            name: 'Charlie',
+            age: 50
+        },
+        available: false,
+        price: '48€',
+        tags: ['html', 'advanced', 'junior', 'mid-senior']
+    },
 ];
 
 
-// Snack 4 - Calcola l’età media:
-// - Creare un array (ages) che contiene le età degli autori dei libri.
-// - Calcola la somma delle età (agesSum) usando reduce.
-// - Stampa in console l’età media degli autori dei libri.
+// Snack 5 (Bonus) - Raccogli i libri:
+// - Usando l'API http://localhost:3333/books/{id} usa la combinazione di .map() e Promise.all(), 
+// per creare una funzione (getBooks) che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
+// Testala con l’array [2, 13, 7, 21, 19] .
 
-const ages = books.map( book => book.author.age);
-console.log(ages);
 
-const agesSum = ages.reduce((acc, age) => acc + age, 0);
-console.log(agesSum);
+const ids = [2, 13, 7, 21, 19];
 
-console.log(`L'età medie è: ${agesSum / ages.length}`);
+async function getBooks(ids) {
+    const apiUrl = 'http://localhost:3333/books/';
+   return Promise.all(
+        ids.map(id => fetch(`${apiUrl}${id}`).then(res => res.json()))
+    );
+}
+getBooks(ids).then(books => console.log(books));
+
 
 
 
